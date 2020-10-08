55abc81b0a52a3dc3bcf48e063d62d2aed3711de
Samuel Alvarez <seam.2194@gmail.com>
2020-10-08 16:27:38 -0500

    Add latest babel support for optional chaining

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,10 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-object-rest-spread']
+          }
         },
       },
 
